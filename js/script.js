b2430fd4208d64f8748e805bf507a2eb26744dd9
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-17 11:54:58 +0300

    Change movements into txns (i.e. transactions)

--- js/script.js
+++ js/script.js
@@ -3,28 +3,28 @@
 // Create accounts
 const account1 = {
   owner: 'Abasa Wandega',
-  movement: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  txns: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interstRate: 1.2, // 1.2%
   pin: 1111,
 };
 
 const account2 = {
   owner: 'Simon Ojok',
-  movement: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  txns: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interstRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
   owner: 'Wilber Natamba',
-  movement: [200, -200, 340, -300, -20, 50, 400, -460],
+  txns: [200, -200, 340, -300, -20, 50, 400, -460],
   interstRate: 0.7,
   pin: 333,
 };
 
 const account4 = {
   owner: 'Pius Omoding',
-  movement: [430, 1000, 700, 50, 90],
+  txns: [430, 1000, 700, 50, 90],
   interstRate: 1,
   pin: 4444,
 };
@@ -43,7 +43,7 @@ const labelSumInterest = select('.summary__value--interest');
 const labelTimer = select('.timer');
 
 const containerApp = select('.app');
-const containerMovements = select('.movements');
+const containerTxns = select('.txns');
 
 const btnLogin = select('.login__btn');
 const btnTransfer = select('.form__btn--transfer');
