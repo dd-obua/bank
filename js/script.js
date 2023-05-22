e20154bf531606d2619b792182cd2d709ab57c1b
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-22 14:34:16 +0300

    If triggered, sort  a copy of  txns and store results into txtns

--- js/script.js
+++ js/script.js
@@ -78,6 +78,8 @@ const displayTrxns = function (txns, sort = false) {
   // Empty transactions container
   containerTxns.innerHTML = '';
 
+  const txtns = sort ? txns.slice().sort((a, b) => a - b) : txns;
+
   txns.forEach(function (txn, i) {
     const type = txn > 0 ? 'deposit' : 'withdrawal';
 
