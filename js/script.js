fa5613db2a056b4c7b8cfcd9f82fb84a7d5eef3f
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-25 12:35:52 +0300

    Round down requested loan to nearest old number

--- js/script.js
+++ js/script.js
@@ -192,7 +192,7 @@ btnLogin.addEventListener('click', function (e) {
   btnLoan.addEventListener('click', function (e) {
     e.preventDefault();
 
-    const amount = Number(inputLoanAmount.value);
+    const amount = Math.floor(inputLoanAmount.value);
     if (amount > 0 && activeAccount.txns.some(txn => txn >= amount * 0.1)) {
       activeAccount.txns.push(amount);
     }
