f5e7e91e7b2a9aab9b5ffc4084c7d2ec7c057250
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-26 12:43:07 +0300

    Caculate number of days passed since the transaction

--- js/script.js
+++ js/script.js
@@ -136,6 +136,8 @@ const formatTxnDate = function (date) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
+  const daysPassed = calcDaysPassed(new Date(), date);
+
   const day = `${date.getDate()}`.padStart(2, 0);
   const month = `${date.getMonth() + 1}`.padStart(2, 0);
   const year = `${date.getFullYear()}`;
