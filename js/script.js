9ac672fee2b0f18a790eb12ff431f64d79032210
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-26 14:17:18 +0300

    Remove else part from  date information logic

--- js/script.js
+++ js/script.js
@@ -105,12 +105,11 @@ const formatTxnDate = function (date) {
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
-  else {
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = `${date.getFullYear()}`;
-    return `${day}/${month}/${year}`;
-  }
+
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = `${date.getFullYear()}`;
+  return `${day}/${month}/${year}`;
 };
 
 // Display transactions
