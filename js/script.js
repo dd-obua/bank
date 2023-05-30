0a184f342235f84f59123d5c4358cb3893bb825e
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-30 14:58:43 +0300

    Remove extra space

--- js/script.js
+++ js/script.js
@@ -99,13 +99,10 @@ const now = new Date();
 const formatTxnDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
-
   const daysPassed = calcDaysPassed(now, date);
-
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
-
   return new Intl.DateTimeFormat(locale).format(date);
 };
 
